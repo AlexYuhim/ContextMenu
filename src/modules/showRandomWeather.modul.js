0b4ed19328adc76e2dc400f8ea5bb9b876a90849
Alexander Yuhimenko <a.yuhim@mail.ru>
2022-11-24 22:12:59 +0300

    обработал запрос от API

--- src/modules/showRandomWeather.modul.js
+++ src/modules/showRandomWeather.modul.js
@@ -18,20 +18,26 @@ export class ModuleRandomWeather extends Module{
                   <h2>RandomWeatherCiti</h2>
                   <h3 class="city-name "></h3class>
                   <h3 class="city-temp"></h3>  
+                  <h3 class="city-discription"></h3>  
+                  <h3 class="city-feels_like"></h3>  
+                  
                 </div>
     `
 
 
     function getRandomWeather(){
 
-      const  URL_API_Weather = `http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=e2df2d1b98fc561c2c721d6d2a82353e`
+      // const  URL_API_Weather = `'https://api.gismeteo.net/v2/weather/current/?latitude=54.35&longitude=52.52'`
+      const  URL_API_Weather = `https://api.openweathermap.org/data/2.5/weather?lat=${getRandomNumber(-90,90)}.${getRandomNumber(0,9999)}&lon=${getRandomNumber(-110,170)}.${getRandomNumber(0,9999)}&lang=ru&units=metric&appid=e2df2d1b98fc561c2c721d6d2a82353e`
       // const  URL_API_Weather = `http://api.weatherapi.com/v1/current.json?key=b9453e9ca4f14ce1be4123538220911&q=${getRandomNumber(-70,70)}.${getRandomNumber(0,9999)},${getRandomNumber(-110,170)}.${getRandomNumber(0,9999)}&aqi=no`
       fetch(URL_API_Weather)
         .then((response)=>response.json())
         .then((city)=>{
           console.log(city)
-          // document.querySelector('.city-name').innerHTML = `Город : ${city.location.name}`;
-          // document.querySelector('.city-temp').innerHTML =`Температура : ${city.current.temp_c} `;
+          document.querySelector('.city-name').innerHTML = `Город : ${city.name}`;
+          document.querySelector('.city-discription').innerHTML = ` : ${city.weather[0].description}`;
+          document.querySelector('.city-feels_like').innerHTML =`ощущается как : ${city.main.feels_like} `;
+          document.querySelector('.city-temp').innerHTML =`Температура : ${city.main.temp} `;
           // console.log('CITY',  city.location.name)
           // console.log("temp_c",  city.current.temp_c)
 
